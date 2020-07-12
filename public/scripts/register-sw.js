369cd340bb489cd6b73e695cc78d67dde81d8d9f
superical <superical@users.noreply.github.com>
2020-07-12 19:19:21 +0800

    Flag to allow disabling of service worker (#1)
    Add flag to allow disabling of service worker registration

--- public/scripts/register-sw.js
+++ public/scripts/register-sw.js
@@ -1,8 +1,25 @@
+/**
+ * If you want to "enable" PWA offline and caching capabilities,
+ * please make sure you set window.ENABLE_SERVICE_WORKER = true
+ *
+ * If you want to "disable" PWA offline and caching capabilities,
+ * please make sure you set window.ENABLE_SERVICE_WORKER = false
+ */
+window.ENABLE_SERVICE_WORKER = true;     // Set this to true to register service worker
+
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((reg) => {
-        console.log('Service worker registered.', reg);
-      });
-  });
+  if (!window.ENABLE_SERVICE_WORKER) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    });
+  } else {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((reg) => {
+          console.log('Service worker registered.', reg);
+        });
+    });
+  }
 }
