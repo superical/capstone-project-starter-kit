d5911a7ddf733fab931adf00d7a1f45556ff6b63
Seth <seth.low@global-fashion-group.com>
2020-07-12 18:55:21 +0800

    update: allow disable service worker registration

--- public/scripts/register-sw.js
+++ public/scripts/register-sw.js
@@ -1,8 +1,22 @@
+/**
+ * If you want to enable PWA offline capabilities and experience,
+ * please make sure you set window.DISABLE_SERVICE_WORKER = false
+ */
+window.DISABLE_SERVICE_WORKER = true;     // Set this to false to register service worker
+
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then((reg) => {
-        console.log('Service worker registered.', reg);
-      });
-  });
+  if (window.DISABLE_SERVICE_WORKER) {
+    navigator.serviceWorker.getRegistrations().then(function (registrations) {
+      for (let registration of registrations) {
+        registration.unregister();
+      }
+    });
+  } else {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/service-worker.js')
+        .then((reg) => {
+          console.log('Service worker registered.', reg);
+        });
+    });
+  }
 }
